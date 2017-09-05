3e57aa9a97c5fca577493b4a6acc81d8efe668e0
Kaelinator <kirkkael@gmail.com>
2017-09-05 15:48:49 -0500

    Now using Tape for unit tests

---
+++ test/quora.test.js
@@ -0,0 +1,18 @@
+
+const tape = require('tape').test
+const quora = require('../index.js')
+
+tape('quora', t => {
+
+	quora('Marcus-Bowdidge-1').then((result) => {
+
+		t.equal(null, result.totalViews, 'returns null for unfilled values')
+		t.end()
+	}).catch((err) => err)
+
+	// quora('Kael-Kirk').then((result) => {
+	//
+	// 	t.equal(null, result.totalViews, 'returns null for unfilled values')
+	// 	t.end()
+	// })
+})
